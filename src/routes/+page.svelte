19df828731e91405c1064c06a6f35fa49de6647b
jgoetz <jacob.goetz@cloudflight.io>
2024-11-28 11:32:51 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+    let a: string;
+
+    $: a, myFunction();
+
+    function myFunction() {
+        console.log("Hello World!")
+    }
+
+</script>
+
+<input bind:value={a}>
